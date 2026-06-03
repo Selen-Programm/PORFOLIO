df97a6af0aad9e4bc3eefc55c96663fe92ed13b2
Selen-Programm <sel.ehagaraztec2@gmail.com>
2026-06-02 23:10:44 -0300

    portafolio design

---
+++ main.js
@@ -0,0 +1,14 @@
+// find elements
+$featurePic = $(".pic")
+$picMessage = $(".picMessage")
+
+// handle hover
+$featurePic.mouseenter(
+function(){
+  $(this).find($picMessage).animate({'top': '30px' }, 'medium');
+});
+
+$featurePic.mouseleave(
+function(){
+  $(this).find($picMessage).animate({'top': '87%' }, 'medium');
+});
